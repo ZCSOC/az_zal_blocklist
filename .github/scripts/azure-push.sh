5c4d154ee78063cdee340dadaf1944e0b834d6c4
Z-IT-INFRA <hosting@zalaris.de>
2025-08-07 16:54:47 +0200

    Update azure-push.sh
    went route to just filter for ":"
    each line with ":" is removed

--- .github/scripts/azure-push.sh
+++ .github/scripts/azure-push.sh
@@ -22,9 +22,16 @@ OLD_OBJECT_ID=$(curl -sS -u :${AZ_PAT} "${REFS_URL}" \
 # # Read and JSON-escape the file content
 # FILE_CONTENT=$(jq -Rs . "${SOURCE_FILE}")
 
-# Read and JSON-escape, removes duplicates and IPv6 from the file content
+# Read and JSON-escape, removes duplicates and IPv6 from the file content - only 3160 IPs left
+# FILE_CONTENT=$(
+#   grep -E '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' "${SOURCE_FILE}" |
+#   sort -u |
+#   jq -Rs .
+# )
+
+# Read and JSON-escape, removes dupes, IPv6 from file content
 FILE_CONTENT=$(
-  grep -E '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' "${SOURCE_FILE}" |
+  grep -v ':' "$SOURCE_FILE" |
   sort -u |
   jq -Rs .
 )
